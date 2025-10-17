0cae63ca0d4e2adddd0519c64447a5dc601a3b5b
Gmoges <156920398+Gmoges@users.noreply.github.com>
2025-10-17 17:18:37 +0300

    final update

--- server/audio_script/audio.py
+++ server/audio_script/audio.py
@@ -1,75 +1,87 @@
-# audio for server side
 
-import socket       # to crete TCP connection
-import pyaudio      # to control audio input and output
-import threading    # To  send & receive at the same time without blocking
+#  1.librarys
 
+import socket           #  used for Network communication
+import pyaudio          #. record and play audio from the microphone and speakers
+import threading        #. multiple tasks at the same time
+from Crypto.Cipher import AES   # inport Advanced Encryption Standard(AES) to decript data
 
-# Audio settings
-CHUNK = 1024      
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100   # in every second 44,100 or 44.1KHz audios are captured from the mic or sent to the speaker.
+#  2. symmetric key (must be same as user side code)
+key = b"This1sH4k3R1o9!!"    #. sed to encrypt/decrypt
+nonce = b"123456789012"      #. random-like number ensuring encryption uniqueness but it's fixed for now!
+cipher_enc = AES.new(key, AES.MODE_EAX, nonce=nonce)    #. encrypt outgoing audio
+cipher_dec = AES.new(key, AES.MODE_EAX, nonce=nonce)    #. decrypt incoming audio
 
+# 3. Audio settings
+FRAMS = 1024                       #. Number of audio frames
+SAMPLE = pyaudio.paInt16           #. 16-bit resolution per audio sample
+CHANNEL = 1
+RATE = 44100                       # CD quality
+
+# 4. printing YMS LOGO
 
 logo = """
-
 __  ____  _______               ___                 ____
  \ \/ /  |/  / __/ ___ ___ _____/ (____    _______ _/ / /
   \  / /|_/ _\ \  / _ `/ // / _  / / _ \  / __/ _ `/ / / 
   /_/_/  /_/___/  \_,_/\_,_/\_,_/_/\___/  \__/\_,_/_/_/  
-                                                         
-
 """
-
-
-
 logo = "\033[34m" + logo + "\033[0m"
-
 print(logo)
 
+PORT = 2119  #Port number the server is runing
 
-PORT = 2119
-# PyAudio
-audio = pyaudio.PyAudio()
-stream_in = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-stream_out = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
+# 5. PyAudio
+audio = pyaudio.PyAudio()        # Initializes the audio system
+stream_in = audio.open(format=SAMPLE, channels=CHANNEL, rate=RATE, input=True, frames_per_buffer=FRAMS)  
 
-# TCP Server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("0.0.0.0", PORT))
+# captures voice
+
+stream_out = audio.open(format=SAMPLE, channels=CHANNEL, rate=RATE, output=True, frames_per_buffer=FRAMS)
+# plays the user’s voice
+
+
+# 6. TCP Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creates a TCP socket
+server.bind(("0.0.0.0", PORT)) # bind in any host and port 2119
 server.listen(1)
 print(" Waiting for connection...")
-conn, addr = server.accept()
-print(f" Connected by {addr}")
+connection, addressUser = server.accept()
+print(f" Connected by {addressUser}")
+
+
 
 def send_audio():
     while True:
         try:
-            data = stream_in.read(CHUNK, exception_on_overflow=False)
-            conn.sendall(data)
+            data = stream_in.read(FRAMS, exception_on_overflow=False) # Continuously geting dta from user
+            ciphertext = cipher_enc.encrypt(data)   # Encrypt before sending
+            connection.sendall(ciphertext) # Sends encrypted audio to server
         except:
             break
 
 def receive_audio():
     while True:
         try:
-            data = conn.recv(CHUNK)
-            if not data:
+            data = connection.recv(2048)                  #reciving up to 2048 bytes
+            if not data:                            # connection closed
                 break
-            stream_out.write(data)
+            plaintext = cipher_dec.decrypt(data)    # Decrypt before playing
+            stream_out.write(plaintext)             # Decrypts the audio
         except:
             break
 
-t1 = threading.Thread(target=send_audio)
-t2 = threading.Thread(target=receive_audio)
+t1 = threading.Thread(target=send_audio)            # Handles sending audio
+t2 = threading.Thread(target=receive_audio)         # Handles receiving audio
 t1.start()
 t2.start()
 
 t1.join()
 t2.join()
 
-conn.close()
+# closed the connection
+
+connection.close()
 stream_in.close()
 stream_out.close()
 audio.terminate()
